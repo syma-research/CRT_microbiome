590d59e4546e3b3c5b707cd16b42cef8b470dbf5
Siyuan Ma <sim653@mail.harvard.edu>
2019-10-26 18:06:01 -0400

    update MCMC with gcoda sampling

--- R/MCMC.R
+++ R/MCMC.R
@@ -1,6 +1,203 @@
-logit <- function(x) log(x) - log(1 - x)
+mcmc_gcoda <- function(p, index,
+                       params, R = 10000) {
+  if(length(index) != length(p) |
+     !is.logical(index) |
+     sum(index) != 1)
+    stop("Index does not follow format!")
+  if(any(p <= 0))
+    stop("p must be positive!")
+  
+  Omega <- params$Omega
+  sigma <- sqrt(solve(Omega)[index, index, drop = TRUE])
+  mu <- params$mu
+  rowsum_Omega <- rowSums(Omega)
+  sum_Omega <- sum(rowsum_Omega)
+  A <- Omega - rowsum_Omega %*% t(rowsum_Omega) / sum_Omega
+  
+  logp <- log(p)
+  l_samples <- list()
+  l_samples[[1]] <- list(logp = logp,
+                         index = index,
+                         sigma = sigma,
+                         mu = mu,
+                         A = A,
+                         logLik = log_dmvnorm(logp - mu, A) - 
+                           log(1 - exp(logp[index])))
+  
+  for(r in (seq_len(R) + 1)) {
+    l_samples[[r]] <- 
+      one_step_gcoda(logp = l_samples[[r - 1]]$logp,
+                     index = index,
+                     sigma = sigma, 
+                     mu = mu, 
+                     A = A,
+                     logLik = l_samples[[r - 1]]$logLik)
+  }
+  
+  return(l_samples[-1])
+}
 
-expit <- function(x) exp(x) / (1 + exp(x))
+one_step_gcoda <- function(logp, index, sigma, mu, A, logLik) {
+  logp_star <- logp
+  while(TRUE) {
+    logp_star[index] <- logp[index] + rnorm(n = 1, sd = sigma)
+    if(logp_star[index] < 0) break
+  }
+  logp_star[!index] <- 
+    logp[!index] + 
+    log(1 - exp(logp_star[index])) - 
+    log(1 - exp(logp[index]))
+  logLik_star <- log_dmvnorm(logp_star - mu, A) - log(1 - exp(logp_star[index]))
+  prop <- exp(logLik_star - logLik) * 
+    pnorm(-logp[index], sd = sigma) / 
+    pnorm(-logp_star[index], sd = sigma)
+    
+  if(runif(1) < prop)
+    return(list(accept = TRUE,
+                prop = prop,
+                logp = logp_star,
+                logLik = logLik_star))
+  else
+    return(list(accept = FALSE,
+                prop = prop,
+                logp = logp,
+                logLik = logLik))
+}
+
+mcmc_copulasso <- function(p, index,
+                           params,
+                           R = 10000) {
+  if(length(index) != length(p) |
+     !is.logical(index) |
+     sum(index) != 1)
+    stop("Index does not follow format!")
+  A <- params$Omega - diag(length(p))
+  u <- p_to_u(p, mu = params$mu, sigma = params$sigma, pi = params$pi)
+  g <- qnorm(u)
+  
+  l_samples <- list()
+  l_samples[[1]] <- list(g = g,
+                         u = u,
+                         p = p,
+                         loglik_g = log_dmvnorm(g, A),
+                         loglik_u = sum(log(u_to_d(u, params$pi))))
+  for(r in (seq_len(R) + 1)) {
+    l_samples[[r]] <- 
+      one_step_copulasso(g = l_samples[[r - 1]]$g,
+                         u = l_samples[[r - 1]]$u,
+                         p = l_samples[[r - 1]]$p,
+                         index = index,
+                         mu = params$mu, sigma = params$sigma, pi = params$pi, 
+                         A = A,
+                         loglik_g = l_samples[[r - 1]]$loglik_g, 
+                         loglik_u = l_samples[[r - 1]]$loglik_u)
+  }
+  
+  return(l_samples[-1])
+}
+
+mcmc2_copulasso <- function(p, index,
+                            params,
+                            steps = 10,
+                            R = 1000,
+                            ncores = 6) {
+  if(length(index) != length(p) |
+     !is.logical(index) |
+     sum(index) != 1)
+    stop("Index does not follow format!")
+  A <- params$Omega - diag(length(p))
+  u <- p_to_u(p, mu = params$mu, sigma = params$sigma, pi = params$pi)
+  g <- qnorm(u)
+  
+  ll_paths <- list()
+  
+  # first, walk steps to generate "center" of MCMC sampling
+  ll_paths[[1]] <- vector("list", steps + 1)
+  ll_paths[[1]][[steps + 1]] <- list(g = g,
+                                     u = u,
+                                     p = p,
+                                     loglik_g = log_dmvnorm(g, A),
+                                     loglik_u = sum(log(u_to_d(u, params$pi))))
+  for(i_step in seq_len(steps)) {
+    ll_paths[[1]][[steps + 1 - i_step]] <- 
+      one_step_copulasso(
+        g = ll_paths[[1]][[steps + 2 - i_step]]$g,
+        u = ll_paths[[1]][[steps + 2 - i_step]]$u,
+        p = ll_paths[[1]][[steps + 2 - i_step]]$p,
+        index = index,
+        mu = params$mu, sigma = params$sigma, pi = params$pi, A = A,
+        loglik_g = ll_paths[[1]][[steps + 2 - i_step]]$loglik_g, 
+        loglik_u = ll_paths[[1]][[steps + 2 - i_step]]$loglik_u)
+  }
+  sample_center <- ll_paths[[1]][[1]]
+  ll_paths[[1]] <- ll_paths[[1]][-1]
+  
+  # Now start from the center, simultaneously sample new samples
+  doParallel::registerDoParallel(cores = ncores)
+  ll_paths_new <- foreach::`%dopar`(
+    foreach::foreach(r = seq_len(R)),
+    {
+      l_samples <- list()
+      l_samples[[1]] <- sample_center
+      for(i_step in seq_len(steps)) {
+        l_samples[[i_step + 1]] <- 
+          one_step_copulasso(
+            g = l_samples[[i_step]][[steps + 2 - i_step]]$g,
+            u = l_samples[[i_step]][[steps + 2 - i_step]]$u,
+            p = l_samples[[i_step]][[steps + 2 - i_step]]$p,
+            index = index,
+            mu = params$mu, sigma = params$sigma, pi = params$pi, A = A,
+            loglik_g = l_samples[[i_step]][[steps + 2 - i_step]]$loglik_g, 
+            loglik_u = l_samples[[i_step]][[steps + 2 - i_step]]$loglik_u)
+      }
+      l_samples <- l_samples[-1]
+      return(l_samples)
+    })
+  doParallel::stopImplicitCluster()
+  
+  ll_paths <- c(ll_paths, ll_paths_new)
+  return(ll_paths[-1])
+}
+
+one_step_copulasso <- function(g, u, p, index,
+                               mu, sigma, pi, A,
+                               loglik_g, loglik_u) {
+  
+  g_star <- g
+  u_star <- u
+  p_star <- p
+  g_star[index] <- g[index] + rnorm(n = 1)
+  u_star[index] <- pnorm(g_star[index])
+  p_star[index] <- u_to_p(u_star[index], 
+                          mu = mu[index], 
+                          sigma = sigma[index], 
+                          pi = pi[index])
+  p_star[!index] <- p[!index] * (1 - p_star[index]) / (1 - p[index])
+  u_star[!index] <- p_to_u(p_star[!index], 
+                           mu = mu[!index], 
+                           sigma = sigma[!index], 
+                           pi = pi[!index])
+  g_star[!index] <- qnorm(u_star[!index])
+  loglik_g_star <- log_dmvnorm(g_star, A)
+  loglik_u_star <- sum(log(u_to_d(u_star, pi)))
+  prop <- exp(loglik_g_star - loglik_g + loglik_u_star - loglik_u)
+  if(runif(1) < prop)
+    return(list(accept = TRUE,
+                prop = prop,
+                g = g_star,
+                u = u_star,
+                p = p_star,
+                loglik_g = loglik_g_star,
+                loglik_u = loglik_u_star))
+  else
+    return(list(accept = FALSE,
+                prop = prop,
+                g = g,
+                u = u,
+                p = p,
+                loglik_g = loglik_g,
+                loglik_u = loglik_u))
+}
 
 p_to_u <- function(p, mu, sigma, pi) {
   to_return <- pi / 2
@@ -35,75 +232,5 @@ u_to_d <- function(u, pi) {
 }
 
 log_dmvnorm <- function(g, A) {
-  return((-t(g) %*% A %*% g / 2)[1, 1])
-}
-
-one_step <- function(g, u, p,
-                     A, 
-                     mu, sigma, pi,
-                     loglik_g, loglik_u) {
-  g_star <- g
-  u_star <- u
-  p_star <- p
-  g_star[1] <- g[1] + rnorm(n = 1)
-  u_star[1] <- pnorm(g_star[1])
-  p_star[1] <- u_to_p(u_star[1], mu = mu[1], sigma = sigma[1], pi = pi[1])
-  p_star[-1] <- p[-1] * (1 - p_star[-1]) / (1 - p[-1])
-  u_star[-1] <- p_to_u(p_star[-1], mu = mu[-1], sigma = sigma[-1], pi = pi[-1])
-  g_star[-1] <- qnorm(u_star[-1])
-  loglik_g_star <- log_dmvnorm(g_star, A)
-  loglik_u_star <- sum(log(u_to_d(u_star, pi)))
-  prop <- exp(loglik_g_star - loglik_g + loglik_u_star - loglik_u)
-  if(runif(1) < prop)
-    return(list(accept = TRUE,
-                prop = prop,
-                g = g_star,
-                u = u_star,
-                p = p_star,
-                loglik_g = loglik_g_star,
-                loglik_u = loglik_u_star))
-  else
-    return(list(accept = FALSE,
-                prop = prop,
-                g = g,
-                u = u,
-                p = p,
-                loglik_g = loglik_g,
-                loglik_u = loglik_u))
-}
-
-metro <- function(p, mu, sigma, pi, Sigma, 
-                  R = 1000, K = 3) {
-  A <- solve(Sigma) - diag(ncol(Sigma))
-  u <- p_to_u(p, mu = mu, sigma = sigma, pi = pi)
-  g <- qnorm(u)
-  
-  doParallel::registerDoParallel(cores = K)
-  l_chains <- foreach::`%dopar%`(
-    foreach::foreach(k = 1:K),
-    {
-      # Initialize p
-      g[1] <- g[1] + rnorm(n = 1)
-      u[1] <- pnorm(g[1])
-      p[1] <- u_to_p(u[1], mu = mu[1], sigma = sigma[1], pi = pi[1])
-      samples <- list()
-      samples[[1]] <- list(g = g,
-                           u = u,
-                           p = p,
-                           loglik_g = log_dmvnorm(g, A),
-                           loglik_u = sum(log(u_to_d(u, pi))))
-      for(r in 2:(R + 1)) {
-        samples[[r]] <- 
-          one_step(g = samples[[r - 1]]$g,
-                   u = samples[[r - 1]]$u,
-                   p = samples[[r - 1]]$p,
-                   A = A, mu = mu, sigma = sigma, pi = pi,
-                   loglik_g = samples[[r - 1]]$loglik_g, 
-                   loglik_u = samples[[r - 1]]$loglik_u)
-      }
-      return(samples)
-    })
-  doParallel::stopImplicitCluster()
-  
-  return(l_chains)
+  return((-t(g) %*% A %*% g / 2)[1, 1, drop = TRUE])
 }
