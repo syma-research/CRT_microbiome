db3189120f87e27f980acd7c60c974f6d6fad029
Siyuan Ma <sim653@mail.harvard.edu>
2019-10-30 23:28:46 -0400

    output zero component indicator for rgcoda as well; some more bugs

--- R/helpers_sim.R
+++ R/helpers_sim.R
@@ -53,7 +53,7 @@ rcopulasso <- function(n, mean, sd, pi, sigma, norm = TRUE) {
            rep(0.0, n))
   
   if(norm)
-    mat_samples <- t(apply(mat_samples, 2, function(x) x / sum(x)))
+    mat_samples <- apply(mat_samples, 1, function(x) x / sum(x))
   
   return(mat_samples)
 }
@@ -73,17 +73,20 @@ rgcoda <- function(n, mean0, mean1, sd0, sd1, pi, sigma) {
     stop("Parameter dimensions must agree!")
   
   # sample marginals
-  mat_marginals <- 
-    vapply(seq_len(length(mean0)),
+  l_marginals <- 
+    lapply(seq_len(length(mean0)),
            function(i)
              rMixLogN_one(n = n,
                           mean0 = mean0[i], mean1 = mean1[i],
                           sd0 = sd0[i], sd1 = sd1[i],
-                          pi = pi[i]),
-           rep(0.0, n))
-  # arrange from smallest to largest for shuffling
-  mat_marginals <- 
-    apply(mat_marginals, 2, function(x) x[order(x)])
+                          pi = pi[i]))
+  mat_marginals <- vapply(l_marginals, 
+                          function(x) x$abd,
+                          rep(0.0, n))
+  mat_ind <- vapply(l_marginals, 
+                    function(x) x$ind,
+                    rep(1, n))
+  mat_order <- apply(mat_marginals, 2, order)
   
   # sample ranks
   mat_rank <- 
@@ -93,17 +96,26 @@ rgcoda <- function(n, mean0, mean1, sd0, sd1, pi, sigma) {
   mat_samples <- 
     vapply(seq_len(length(mean0)),
            function(i)
-             mat_marginals[, i, drop = TRUE][mat_rank[, i, drop = TRUE]],
+             mat_marginals[, i, drop = TRUE][mat_order[, i, drop = TRUE]][
+               mat_rank[, i, drop = TRUE]],
+           rep(0.0, n))
+  mat_ind <- 
+    vapply(seq_len(length(mean0)),
+           function(i)
+             mat_ind[, i, drop = TRUE][mat_order[, i, drop = TRUE]][
+               mat_rank[, i, drop = TRUE]],
            rep(0.0, n))
   
   
-  mat_samples <- t(apply(mat_samples, 2, function(x) x / sum(x)))
+  mat_samples <- apply(mat_samples, 1, function(x) x / sum(x))
   
-  return(mat_samples)
+  return(list(abd = mat_samples,
+              ind = t(mat_ind)))
 }
 
 rMixLogN_one <- function(n, mean0, mean1, sd0, sd1, pi) {
   ind <- rbinom(n = n, size = 1, prob = pi)
-  return(exp(rnorm(n = n, mean = mean0, sd = sd0) * (1 - ind) +
-               rnorm(n = n, mean = mean1, sd = sd1) * ind))
+  return(list(abd = exp(rnorm(n = n, mean = mean0, sd = sd0) * (1 - ind) +
+                          rnorm(n = n, mean = mean1, sd = sd1) * ind),
+              ind = ind))
 }
